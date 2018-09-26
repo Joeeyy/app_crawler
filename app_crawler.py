bd29e1d3ca092a9b868c7cd10026a5885f6f33fc
Joeeyy <joeeeee@foxmail.com>
2018-09-26 13:47:39 +0800

    get categories&genres' info. get catetory names and ids

--- app_crawler.py
+++ app_crawler.py
@@ -1,7 +1,73 @@
 #coding=utf8
+import requests
+import json
+
+'''
+[官方分类显示列表](https://affiliate.itunes.apple.com/resources/documentation/genre-mapping/)
+	[app search](http://itunes.apple.com/search?term=google&country=us&entity=software)
+	param 1: term，搜索关键词
+	param 2: country, 搜索的市场
+	param 3: entity, 限定为software
+
+	爬虫思路：获取所有分类（genre），按照分类中字母顺序爬取。爬取到的app应该只是名字，注意记录重复的情况。
+	然后根据名称查询到app具体内容。主要是bundle id，图标也是考虑内容之一。
+'''
+
+'''
+本爬虫目标位app_store中的app信息，由于app_store隶属于itunes_store，这里是用的是itunes_store提供的文档进行开发。
+itunes_store按照类目分为以下若干类（见链接：https://affiliate.itunes.apple.com/resources/documentation/genre-mapping/）。
+分类级别如下：
+目录(category) -> 分类(genre) -> 子类(subgenre)
+分类包括分类名（genre_name），分类id（genre_id）
+分类结构化内容可以由https://itunes.apple.com/WebObjects/MZStoreServices.woa/ws/genres得到。
+'''
+
+genre_service_url = "https://itunes.apple.com/WebObjects/MZStoreServices.woa/ws/genres"
+cgInfoFile = "./cgInfoFile.txt"
+# 在进行真正的爬去工作之前，应该根据getCategories()确定存在的目录，爬取指定的目录，这里关注app store，然后需要获取app store对应的category id。
+targetCategory = "App Store"
+
+#cg: categories and genres
+def fetch_cgInfo():
+	response = requests.get(genre_service_url)
+	print('status code: %d\nresponse encoding: %s'%(response.status_code, response.encoding))
+	
+	f = open(cgInfoFile, 'w')
+	f.write(response.text)
+	f.close()
+
+# cgInfo here is a json_str
+def read_cgInfo():
+	f = open(cgInfoFile,'r')
+	cgInfo = f.read()
+	f.close()
+	
+	return cgInfo
+
+# returns a dict, in which cate_name is the key, and id is the value
+def getCategories(cg_json=None):
+	category_dict = {}
+	if cg_json == None:
+		return category_dict
+
+	for key in cg_json.keys():
+		category_dict[cg_json[key]['name']] = key
+
+	return category_dict
+
+
+
 
 def main():
-	print "app crawler of apple app store ..."
+	print("app crawler of apple app store ...")
+	cg_json_str = read_cgInfo()
+	cg_json = json.loads(cg_json_str)
+	
+	category_dict = getCategories(cg_json)
+	targetCategory_id = category_dict[targetCategory]
+	print(targetCategory_id)
+
+	
 
 if __name__ == '__main__':
 	main()
